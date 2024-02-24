8c86dbb0d476d08492470be34449a569eac8b1a6
Uriel Camargo <uriecaba@gmail.com>
2024-02-24 17:19:07 -0500

    se  actualiza clase mail para detectar como la ejecuta V9

--- system/library/mail/mail.php
+++ system/library/mail/mail.php
@@ -115,7 +115,7 @@ class Mail {
             $header = 'From: webmaster@example.com' . "\r\n" .
                 'Reply-To: webmaster@example.com' . "\r\n" .
                 'X-Mailer: PHP/' . phpversion();
-            mail($to, $this->option['subject'], $message, $header);
+            mail($to, '=?UTF-8?B?' . base64_encode($this->option['subject']) . '?=', $message);
 			return mail($to, '=?UTF-8?B?' . base64_encode($this->option['subject']) . '?=', $message, $header);
         }
 	}
