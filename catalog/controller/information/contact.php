afaf6407fbc68b8e503eb32314cbb533c727c2aa
Uriel Camargo <uriecaba@gmail.com>
2024-02-24 16:20:38 -0500

    imprimir correo

--- catalog/controller/information/contact.php
+++ catalog/controller/information/contact.php
@@ -154,6 +154,7 @@ class Contact extends \Opencart\System\Engine\Controller {
 					'smtp_timeout'  => $this->config->get('config_mail_smtp_timeout')
 				];
 
+                echo $this->config->get('config_email');
 				$mail = new \Opencart\System\Library\Mail($this->config->get('config_mail_engine'), $mail_option);
 				$mail->setTo($this->config->get('config_email'));
 				// Less spam and fix bug when using SMTP like sendgrid.
